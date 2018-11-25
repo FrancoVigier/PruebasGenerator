06e82e25d8584d9247dae7a1cdc18762bd20fc99
Emmanuel M <emmanuelbendavid@gmail.com>
2018-11-25 03:41:51 -0300

    Add if is all correct or no correct is correct answer to ProtoPregunta

--- src/preprocess/ProtoPregunta.php
+++ src/preprocess/ProtoPregunta.php
@@ -9,16 +9,21 @@ class ProtoPregunta {
 	public $HAS_NO_CORRECT = false;
 	public $HAS_SPECIAL_ALL = NULL;
 	public $HAS_SPECIAL_NO_CORRECT = NULL;
+	public $IS_ALL_CORRECT = false;
+	public $IS_NO_CORRECT = false;
 
 	public function __construct(
 			string $pregunta, array $respuestas, bool $hasAllCorrect,
-			bool $hasNoCorrect, string $hasSpecialAll, string $hasSpecialNoCorrect) {
+			bool $hasNoCorrect, string $hasSpecialAll, string $hasSpecialNoCorrect,
+			bool $isAllCorrect, bool $isNoCorrect) {
 		$this->PREGUNTA = $pregunta;
 		$this->RESPUESTAS = $respuestas;
 		$this->HAS_ALL_CORRECT = $hasAllCorrect;
 		$this->HAS_NO_CORRECT = $hasNoCorrect;
 		$this->HAS_SPECIAL_ALL = $hasSpecialAll;
 		$this->HAS_SPECIAL_NO_CORRECT = $hasSpecialNoCorrect;
+		$this->IS_ALL_CORRECT = $isAllCorrect;
+		$this->IS_NO_CORRECT = $isNoCorrect;
 	}
 
 	function __clone() {
